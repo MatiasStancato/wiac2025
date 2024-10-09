39c18d072ef31cf6c937756ffdcafa2e19621cf9
MatiasStancato <stancato.m@gmail.com>
2024-10-09 22:47:54 +0200

    modController+modStyle

--- controllers/mpController.js
+++ controllers/mpController.js
@@ -1,102 +1,144 @@
 //SDK de Mercado pago
 import { MercadoPagoConfig, Preference } from "mercadopago";
+
 //agrega credenciales
-
 const client = new MercadoPagoConfig({ accessToken: process.env.ACCESSTOKEN });
-
 const preference = new Preference(client);
 
-let preferencesStore = {};
+
+//usar redis
+// import { createClient } from 'redis';
+//Crear un cliente Redis
+// const redisClient = createClient({
+//     password: 'upTkxydgC7oor6RcRQZfXF5ihonoaWE9',
+//     socket: {
+//         host: '127.0.0.1',
+//         port: 6389
+//     }
+// });
+
+// redisClient.on('error', (err) => console.log('Redis Client Error', err));
+
+//Conectar a Redis
+//  await redisClient.connect();
+
+
+import NodeCache from "node-cache";
+
+const myCache = new NodeCache( {stdTTL:3600});
 
 //ruta create preference
 export const createPreference = async (req, res) => {
     try {
-    const { orderData, formData } = req.body;
-    const body = {
+        const { orderData, formData } = req.body;
 
-        items: [
-        {
-            title: orderData.title,
-            quantity: Number(orderData.quantity),
-            unit_price: Number(orderData.price),
-            currency_id: "ARS",
-        },
-        ],
-        back_urls: {
-        success: "https://wiac2025.vercel.app/sucess",
-        failure: "https://wiac2025.vercel.app/inscription",
-        pending: "https://wiac2025.vercel.app/inscription",
-        },
-        auto_return: "approved",
-        notification_url: "https://wiac2025.vercel.app/webhook",
-    };
+        const body = {
+            items: [
+                {
+                    title: orderData.title,
+                    quantity: Number(orderData.quantity),
+                    unit_price: Number(orderData.price),
+                    currency_id: "ARS",
+                },
+            ],
+            back_urls: {
+                success: "https://wiac2025.vercel.app/sucess",
+                failure: "https://wiac2025.vercel.app/inscription",
+                pending: "https://wiac2025.vercel.app/inscription",
+            },
+            auto_return: "approved",
+            notification_url: "https://wiac2025.vercel.app/webhook",
+        };
+        
+        const result = await preference.create({ body });
+        
+        myCache.set("FD",formData);
 
-    const result = await preference.create({ body });
 
-    preferencesStore = formData;
-    console.log('resultaO:',result);
-    console.log("formData", preferencesStore);
+        
 
-    res.json({
-        id: result.id,
-    });
+       // Guardar el formData en Redis usando el customId como clave
+    // await redisClient.setEx(customId, 3600, JSON.stringify(formData)); // Expira en 1 hora
+
+        res.json({id: result.id,});
     } catch (error) {
-    console.log(error);
-    res.status(500).json({
-        error: "Error al crear la preferencia",
-    });
-    
+        console.log(error);
+        res.status(500).json({
+            error: "Error al crear la preferencia",
+        });
     }
 };
 
 // ruta webhook
-export const webhook =  async function (req, res) {
+export const webhook = async function (req, res) {
     const paymentId = req.query["data.id"];
-console.log('preferenceStoredelwebhook',preferencesStore);
 
     try {
-    // Obtener el estado del pago desde MercadoPago
-    const response = await fetch(
-        `https://api.mercadopago.com/v1/payments/${paymentId}`,
-        {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            'Authorization': `Bearer ${client.accessToken}`
-        }
-        }
-    );
+        // Obtener el estado del pago desde MercadoPago
+        
+        const response = await fetch(
+            `https://api.mercadopago.com/v1/payments/${paymentId}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${client.accessToken}`,
+                },
+            }
+        );
 
-    if (response.ok) {
-        const data = await response.json();
-        const status = data.status;
-        const statusdetail = data.status_detail;
-        console.log("response", response);
-        console.log("status:", status);
-        console.log("statusDetail", statusdetail);
+        const DataToSend = myCache.get("FD");
 
-        if (status === "approved") {
-        console.log("data", data);
+        console.log("DataToSend antes de enviarlo:", DataToSend);
 
-        try {
-            const response2 = await fetch("https://wiac2025.vercel.app/save", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(preferencesStore), // Enviando preferencesStore al backend
-            });
-            res.status(200).send("Datos guardados correctamente")
-        } catch (error) {
-            console.error("Error en el fetch:", error);
+        // Verificar si formData sigue disponible en el cache
+        if (!DataToSend) {
+            console.error("Error: No se encontró formData en cache");
+            return res.sendStatus(500);  // Error si los datos no están
         }
-        } else {
-        console.log("Error del response");
+            
+        if (response.ok) {
+            const data = await response.json();
+            const status = data.status;
+
+
+            if (status === "approved") {
+                console.log("Pago aprobado");
+
+                // Recuperar el customId desde los metadatos
+                // const customId = data.metadata.customId;
+                // const formDataString = await redisClient.get(customId);
+
+                // if (!formDataString) {
+                //     console.error("No se encontró formData en cache");
+                //     return res.sendStatus(500);
+                // }
+                // const formData = JSON.parse(formDataString);
+                // console.log('FormData del webhook, ',formDataString);
+                    
+
+                try {
+                    
+                    const response2 = await fetch(
+                        "https://wiac2025.vercel.app//save",
+                        {
+                            method: "POST",
+                            headers: {
+                                "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify(DataToSend), // Enviando preferencesStore al backend
+                        }
+                    );
+                } catch (error) {
+                    console.error("Error en el fetch:", error);
+                }
+            } else {
+                console.log("Error del response");
+            }
         }
-    }
-    res.sendStatus(200); // Respuesta exitosa al webhook
+        res.sendStatus(200); // Respuesta exitosa al webhook
     } catch (error) {
-    console.error("Error en el webhook de MercadoPago:", error);
-    res.sendStatus(500);
+        console.error("Error en el webhook de MercadoPago:", error);
+        res.sendStatus(500);
     }
 };
--- public/js/mpCheckoutBtn.js
+++ public/js/mpCheckoutBtn.js
@@ -1,78 +1,78 @@
 
 const mp = new MercadoPago('TEST-5d3cd7c6-733c-44b3-bc5d-2049e9a0d72c', { locale: "en-US" });
-            const form = document.getElementById('registration-form');
-            document.getElementById('checkout-btn').addEventListener('click', async () => {
-                
-            //  Validar el formulario
-                if (!form.checkValidity()) {
-                    const errorMessages = [];
-                    const inputs = form.querySelectorAll('input, select');
-        
-                    inputs.forEach(input => {
-                        if (!input.validity.valid) {
-                            errorMessages.push(`${input.name} IS INVALID.`);
-                        }
-                    });
-        
-                    alert(errorMessages.join('\n'));
-                    return;
-                }
-        
-        try{
-            // Capturar los datos del formulario
-            const formData = {
-                Firstname: form.querySelector('input[name="Firstname"]').value,
-                Lastname: form.querySelector('input[name="Lastname"]').value,
-                Email: form.querySelector('input[name="Email"]').value,
-                Federation: form.querySelector('select[name="Federation"]').value,
-                Country: form.querySelector('input[name="Country"]').value,
-                Ageclass: form.querySelector('select[name="Ageclass"]').value,
-                Gender: form.querySelector('select[name="Gender"]').value,
-                Bowtype: form.querySelector('select[name="Bowtype"]').value,
-                Target: form.querySelector('select[name="Target"]').value,
-                Mssg:form.querySelector('textarea[name="mssg"]').value,
-             };
-            // Crea la orden para la preferencia de MP    
-            const orderData = { 
-                title : 'Inscripcion WIAC 2025',
-                quantity:1,
-                price: 500,
-            };
-            // Crea un objeto con los das dos constantes
-            const dataToSend = {
-                orderData,
-                formData,
-            };
-    
-            const response = await fetch("/create_preference",{
-            method:'POST',
-            headers:{
-                "Content-Type":"application/json",
-            },
-            body:JSON.stringify(dataToSend),
-            
-        });
-        
-        const preference = await response.json();
-    
-        createCheckoutButton(preference.id);
-        } catch(error){
-            alert("error al crear la preferencia de pago")
-        }
-    })
-    
-        const createCheckoutButton = (preferenceId)=>{
-    
-            const bricksBuilder = mp.bricks();
-    
-            const renderComponent = async () =>{
-                if (window.checkoutButton) window.checkoutButton.unmount();
-                            
-                window.checkoutButton = await bricksBuilder.create("wallet","wallet_container",{
-                    initialization: {
-                        preferenceId: preferenceId,
-                    }
-                })
+const form = document.getElementById('registration-form');
+document.getElementById('checkout-btn').addEventListener('click', async () => {
+
+    //  Validar el formulario
+    if (!form.checkValidity()) {
+        const errorMessages = [];
+        const inputs = form.querySelectorAll('input, select');
+
+        inputs.forEach(input => {
+            if (!input.validity.valid) {
+                errorMessages.push(`${input.name} IS INVALID.`);
             }
-            renderComponent();
-        }
+        });
+
+        alert(errorMessages.join('\n'));
+        return;
+    }
+
+    try {
+        // Capturar los datos del formulario
+        const formData = {
+            Firstname: form.querySelector('input[name="Firstname"]').value,
+            Lastname: form.querySelector('input[name="Lastname"]').value,
+            Email: form.querySelector('input[name="Email"]').value,
+            Federation: form.querySelector('select[name="Federation"]').value,
+            Country: form.querySelector('input[name="Country"]').value,
+            Ageclass: form.querySelector('select[name="Ageclass"]').value,
+            Gender: form.querySelector('select[name="Gender"]').value,
+            Bowtype: form.querySelector('select[name="Bowtype"]').value,
+            Target: form.querySelector('select[name="Target"]').value,
+            Mssg: form.querySelector('textarea[name="mssg"]').value,
+        };
+        // Crea la orden para la preferencia de MP    
+        const orderData = {
+            title: 'Inscripcion WIAC 2025',
+            quantity: 1,
+            price: 500,
+        };
+        // Crea un objeto con los das dos constantes
+        const dataToSend = {
+            orderData,
+            formData,
+        };
+
+        const response = await fetch("/create_preference", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(dataToSend),
+
+        });
+
+        const preference = await response.json();
+
+        createCheckoutButton(preference.id);
+    } catch (error) {
+        alert("error al crear la preferencia de pago")
+    }
+})
+
+const createCheckoutButton = (preferenceId) => {
+
+    const bricksBuilder = mp.bricks();
+
+    const renderComponent = async () => {
+        if (window.checkoutButton) window.checkoutButton.unmount();
+
+        window.checkoutButton = await bricksBuilder.create("wallet", "wallet_container", {
+            initialization: {
+                preferenceId: preferenceId,
+            }
+        })
+    }
+    renderComponent();
+}
